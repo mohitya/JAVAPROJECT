24097ec0f8e19f5e6bf51dc88f1aa9b3ad9d706a
mohitya <my0434599@gmail.com>
2020-02-04 11:57:13 +0530

    done

--- Demo.java
+++ Demo.java
@@ -5,8 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AppController {
+@Autowired
+private IStdService service;
+
 @RequestMapping("/all")
 	public String showAll() {
 	return "AllPage";
+
+
 }
 }
---
+++ Test.java
@@ -0,0 +1,28 @@
+
+package com.app;
+
+class Test
+{
+
+public void show(int a,int b)
+{
+
+
+int c=a+b;
+
+
+}
+
+
+
+public static void main(String []args)
+{
+
+Test t=new Test();
+
+
+t.show(10,23);
+
+
+System.out.println("done");
+}
