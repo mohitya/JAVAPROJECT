a4a2d4bfda1cc0b55528519621e177f5ca84a128
mohitya <my0434599@gmail.com>
2020-02-04 11:54:26 +0530

    done

---
+++ BOOTMVC2EXAMPLE/src/main/java/in/nit/Bootmvc2ExampleApplication.java
@@ -0,0 +1,13 @@
+package in.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootmvc2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootmvc2ExampleApplication.class, args);
+	}
+
+}
